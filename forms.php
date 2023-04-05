1d2ad17e51b8326c9df144a7a75757e3f8f90c7b
bengs <bengs@student.cs.ubc.ca>
2023-04-04 22:57:07 -0700

    refactoring styles and navbar

---
+++ forms.php
@@ -0,0 +1,49 @@
+<div id="company-navigation-bar" class="topnav">
+
+    <!--Company logo-->
+    <div class="topnav-centered">
+        <a href="https://www.students.cs.ubc.ca/~bengs/oracle-test.php"><img src="artifacts/companalytics.png" alt="Companalytics"></a>
+    </div>
+
+    <!--This form is hidden from view, but submitted as HTTP POST when link below is pressed.-->
+    <!--The form is prefilled with values.-->
+    <!--When this form is submitted, the file under action will be called with the array $_POST containing the string "showIndustriesTable"-->
+    <!--INDUSTRIES-->
+    <form method="POST" id="showIndustries" action="oracle-test.php" style="display: none;">
+        <input type="hidden" id="showIndustriesTable" value="showIndustriesTable" name="showIndustriesTable">
+        <p><input type="submit" value="industry" name="showIndustry"></p>
+    </form>
+
+    <!--This is the clickable text that the user can see and press to submit the above hidden form-->
+    <?= '<a href="#" onclick="document.getElementById(\'showIndustries\').submit(); ">Industries</a>'; ?>
+
+    <!--INVESTOR-->
+    <form method="POST" id="showInvestors" action="oracle-test.php" style="display: none;">
+        <input type="hidden" id="showInvestorsTable" value="showInvestorsTable" name = "showInvestorsTable">
+        <input type="submit" value="investor" name="showInvestor">
+    </form>
+
+    <?= '<a href="#" onclick="document.getElementById(\'showInvestors\').submit();">Investors</a>'; ?>
+
+    <!--COMPANIES-->
+    <form method="POST" id="showCompanies" action="oracle-test.php" style="display: none;">
+        <input type="hidden" id="showCompaniesTable" value="showCompaniesTable" name="showCompaniesTable">
+        <p><input type="submit" value="company" name="showCompany"></p>
+    </form>
+
+    <?= '<a href="#" onclick="document.getElementById(\'showCompanies\').submit(); ">Companies</a>'; ?>
+
+     <!--MANAGE-->
+     <form method="POST" id="managePage" action="manage.php" style="display: none;">
+        <input type="hidden" id="goToManage" value="manage" name="goToManage">
+        <p><input type="submit" value="manage" name="manage"></p>
+    </form>
+
+    <?= '<a href="#" onclick="document.getElementById(\'managePage\').submit(); ">Manage</a>'; ?>
+
+    <div class="topnav-right">
+        <?= '<a>Contact Us</a>'; ?>
+        <?= '<a>About</a>'; ?>
+    </div>
+    
+</div>
--- oracle-test.php
+++ oracle-test.php
@@ -6,62 +6,15 @@
 
 <html>
     <head>
-        <link rel="stylesheet" href="styles.css"/>
+        <link rel="stylesheet" type="text/css" href="styles.css"/>
         <title>Companalytics</title>
         <link rel="icon" type="image/x-icon" href="artifacts/104663.png">
     </head>
 
-    <body style = "margin: 0px;">
+    <body>
 
-        <div id="company-navigation-bar" class="topnav">
-
-            <!--Company logo. Currently, pressing it leads to illegal URL-->
-            <div class="topnav-centered">
-                <a href = "https://www.students.cs.ubc.ca/~CWL/oracle-test.php"><img src="artifacts/companalytics.png" alt="Companalytics"></a>
-            </div>
-
-            <!--This form is hidden from view, but submitted as HTTP POST when link below is pressed.-->
-            <!--The form is prefilled with values.-->
-            <!--When this form is submitted, the file under action will be called with the array $_POST containing the string "showIndustriesTable"-->
-            <!--INDUSTRIES-->
-            <form method="POST" id="showIndustries" action="oracle-test.php" style="display: none;">
-                <input type="hidden" id="showIndustriesTable" value="showIndustriesTable" name="showIndustriesTable">
-                <p><input type="submit" value="industry" name="showIndustry"></p>
-            </form>
-
-            <!--This is the clickable text that the user can see and press to submit the above hidden form-->
-            <?= '<a href="#" onclick="document.getElementById(\'showIndustries\').submit(); ">Industries</a>'; ?>
-
-            <!--INVESTOR-->
-            <form method="POST" id="showInvestors" action="oracle-test.php" style="display: none;">
-                <input type="hidden" id="showInvestorsTable" value="showInvestorsTable" name = "showInvestorsTable">
-                <input type="submit" value="investor" name="showInvestor">
-            </form>
-
-            <?= '<a href="#" onclick="document.getElementById(\'showInvestors\').submit();">Investors</a>'; ?>
-
-            <!--COMPANIES-->
-            <form method="POST" id="showCompanies" action="oracle-test.php" style="display: none;">
-                <input type="hidden" id="showCompaniesTable" value="showCompaniesTable" name="showCompaniesTable">
-                <p><input type="submit" value="company" name="showCompany"></p>
-            </form>
-
-            <?= '<a href="#" onclick="document.getElementById(\'showCompanies\').submit(); ">Companies</a>'; ?>
-
-             <!--MANAGE-->
-             <form method="POST" id="managePage" action="manage.php" style="display: none;">
-                <input type="hidden" id="goToManage" value="manage" name="goToManage">
-                <p><input type="submit" value="manage" name="manage"></p>
-            </form>
-
-            <?= '<a href="#" onclick="document.getElementById(\'managePage\').submit(); ">Manage</a>'; ?>
-
-            <div class="topnav-right">
-                <?= '<a>Contact Us</a>'; ?>
-                <?= '<a>About</a>'; ?>
-            </div>
-            
-        </div>
+        <!-- Import the navbar and the hidden HTML forms-->
+        <?php include('forms.php');?>
 
         <div id = "all-content">
 
